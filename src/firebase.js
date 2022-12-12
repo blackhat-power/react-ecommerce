fedba0afb5ab1d4b168e9733fbab0a976444896a
blackhat-power <abdultarickh@gmail.com>
2022-12-13 01:04:17 +0300

    added firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCecarTsOBRmZ11scZLK3b_Ehj9XR5FASo",
+    authDomain: "e-commerce-5ecdb.firebaseapp.com",
+    projectId: "e-commerce-5ecdb",
+    storageBucket: "e-commerce-5ecdb.appspot.com",
+    messagingSenderId: "213606196616",
+    appId: "1:213606196616:web:afeac536f6b7fdc6a0463d",
+    measurementId: "G-E7RYZWZBB4"
+  };
