4bed249e43509006fc32fbe9ee84bdc9068177ba
blackhat-power <abdultarickh@gmail.com>
2022-12-13 11:33:18 +0300

    header mock up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-    <h1>tariq</h1>
+    <Header/>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import companyLogo from "./assets/images/amazon_lit.png";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+<img className='header_logo' src={ companyLogo } alt="" />
+
+<div className='header_search'>
+    <input type="text" className='header_searchInput' name="" value="" />
+    <SearchIcon className='header_searchIcon'/>
+</div>
+
+<div className='header_nav'>
+    <div className='header_option'>
+
+        <span className='header_optionLineOne'> Hello Guest </span>
+        <span className='header_optionLineTwo'> Sign In </span>
+
+    </div>
+    <div className='header_option'>
+
+    <span className='header_optionLineOne'> Returns  </span>
+    <span className='header_optionLineTwo'> & Orders </span>
+
+    </div>
+    <div className='header_option'>
+        
+    <span className='header_optionLineOne'> Your  </span>
+    <span className='header_optionLineTwo'> Prime </span>
+    </div>
+
+    <div className='header_optionBasket'>
+<ShoppingBasketIcon/>
+<span className='header_optionLineTwo header_basketCount'>0</span>
+        </div>
+    
+
+</div>
+
+</div>
+
   )
 }
 
